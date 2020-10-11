a3b43f7cb2fd56eb27ba385e46492233becfdd93
canji53 <canji.preengineer@gmail.com>
2020-10-11 18:45:11 +0900

    :recycle: eslintでlint,formatter

--- render/app.js
+++ render/app.js
@@ -1,20 +1,23 @@
-'use strict'
+"use strict"
 
-const path = require('path')
-const { loadNuxt } = require('nuxt')
+const path = require("path")
+const { loadNuxt } = require("nuxt")
 
-const express = require('express')
+const express = require("express")
 const app = express()
 
-const awsServerlessExpress = require('aws-serverless-express')
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const awsServerlessExpress = require("aws-serverless-express")
+const awsServerlessExpressMiddleware = require("aws-serverless-express/middleware")
 
 app.use(awsServerlessExpressMiddleware.eventContext())
-app.use('/_nuxt', express.static(path.join(__dirname, '.nuxt', 'dist', 'client')))
-app.use('/static', express.static(path.join(__dirname, 'app', 'static')))
+app.use(
+  "/_nuxt",
+  express.static(path.join(__dirname, ".nuxt", "dist", "client"))
+)
+app.use("/static", express.static(path.join(__dirname, "app", "static")))
 
 async function start() {
-  const nuxt = await loadNuxt('start')
+  const nuxt = await loadNuxt("start")
   app.use(nuxt.render)
   return app
 }
